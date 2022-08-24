5e5da8196c8c6a6d98419036ed41736e00f69e3b
matias <matias3282@hotmail.com>
2022-08-24 19:28:14 -0300

    third commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /**@type {HTMLCanvasElement}*/
 import img1 from "../img/raven.png";
+import img2 from "../img/boom.png";
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
@@ -10,6 +12,7 @@ collisionCanvas.width = window.innerWidth;
 collisionCanvas.height = window.innerHeight;
 
 let score = 0;
+let gameOver = false;
 ctx.font = "50px Impact";
 
 let timeToNextRaven = 0;
@@ -25,6 +28,7 @@ function createImage(imageSrc) {
 }
 
 let ravensImage = createImage(img1);
+let boomImage = createImage(img2);
 
 class Raven {
   constructor() {
@@ -55,6 +59,7 @@ class Raven {
       "," +
       this.randomColors[2] +
       ")";
+    this.hasTrail = Math.random() > 0.5;
   }
   update(deltaTime) {
     if (this.y < 0 || this.y > canvas.height - this.height) {
@@ -68,10 +73,16 @@ class Raven {
       if (this.frame > this.maxFrame) this.frame = 0;
       else this.frame++;
       this.timeSinceFlap = 0;
+      if (this.hasTrail) {
+        for(let i = 0; i < 5; i++){
+          particles.push(new Particle(this.x, this.y, this.width, this.color));
+        }
+      }
     }
+    if (this.x < 0 - this.width) gameOver = true;
   }
   draw() {
-      collisionCtx.fillStyle = this.color;
+    collisionCtx.fillStyle = this.color;
     collisionCtx.fillRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       ravensImage,
@@ -87,6 +98,76 @@ class Raven {
   }
 }
 
+let explosions = [];
+
+class Explosion {
+  constructor(x, y, size) {
+    this.image = boomImage;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.size = size;
+    this.x = x;
+    this.y = y;
+    this.frame = 0;
+    this.sound = new Audio();
+    this.sound.src = "../sound/boom.mp3";
+    this.timeSinceLastFrame = 0;
+    this.frameInterval = 250;
+    this.markedForDeletion = false;
+  }
+  update(deltaTime) {
+    if (this.frame === 0) this.sound.play();
+    this.timeSinceLastFrame += deltaTime;
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++;
+      this.timeSinceLastFrame = 0;
+      if (this.frame > 5) this.markedForDeletion = true;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y - this.size / 4,
+      this.size,
+      this.size
+    );
+  }
+}
+
+let particles = [];
+
+class Particle {
+  constructor(x, y, size, color) {
+    this.size = size;
+    this.x = x + this.size / 2;
+    this.y = y + this.size / 3;
+    this.radius = (Math.random() * this.size) / 10;
+    this.maxRadius = Math.random() * 20 + 35;
+    this.markedForDeletion = false;
+    this.speedX = Math.random() * 1 + 0.5;
+    this.color = color;
+  }
+  update() {
+    this.x += this.speedX;
+    this.radius += 0.5;
+    if (this.radius > this.maxRadius - 5) this.markedForDeletion = true;
+  }
+  draw() {
+    ctx.save();
+    ctx.globalAlpha = 1 - this.radius/this.maxRadius
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+  }
+}
+
 function drawScore() {
   ctx.fillStyle = "black";
   ctx.fillText("Score: " + score, 50, 75);
@@ -94,8 +175,39 @@ function drawScore() {
   ctx.fillText("Score: " + score, 55, 80);
 }
 
+function drawGameOver() {
+  ctx.textAlign = "center";
+  ctx.fillStyle = "black";
+  ctx.fillText(
+    "GAME OVER, your score is " + score,
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  ctx.fillStyle = "white";
+  ctx.fillText(
+    "GAME OVER, your score is " + score,
+    canvas.width / 2 + 5,
+    canvas.height / 2 + 5
+  );
+}
+
 addEventListener("click", function (e) {
   const detectedPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
+  console.log(detectedPixelColor);
+  const pc = detectedPixelColor.data;
+  ravens.forEach((object) => {
+    if (
+      object.randomColors[0] === pc[0] &&
+      object.randomColors[1] === pc[1] &&
+      object.randomColors[2] === pc[2]
+    ) {
+      // collision detected
+      object.markedForDeletion = true;
+      score++;
+      explosions.push(new Explosion(object.x, object.y, object.width));
+      console.log(explosions);
+    }
+  });
 });
 
 function animate(timestamp) {
@@ -107,15 +219,20 @@ function animate(timestamp) {
   if (timeToNextRaven > ravenInterval) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
-    ravens.sort(function(a,b){
-        return a.width - b.width;
-    })
+    ravens.sort(function (a, b) {
+      return a.width - b.width;
+    });
   }
   drawScore();
-  [...ravens].forEach((object) => object.update(deltaTime));
-  [...ravens].forEach((object) => object.draw());
+  [...particles, ...ravens, ...explosions].forEach((object) =>
+    object.update(deltaTime)
+  );
+  [...particles, ...ravens, ...explosions].forEach((object) => object.draw());
   ravens = ravens.filter((object) => !object.markedForDeletion);
+  explosions = explosions.filter((object) => !object.markedForDeletion);
+  particles = particles.filter((object) => !object.markedForDeletion);
 
-  requestAnimationFrame(animate);
+  if (!gameOver) requestAnimationFrame(animate);
+  else drawGameOver();
 }
 animate(0);
